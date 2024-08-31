cb09e0d6c13e46a3c26fd6626a34bbfe235709dc
mawira <mawira@students.uonbi.ac.ke>
2024-08-31 11:22:09 +0300

    strore

--- biz/app.py
+++ biz/app.py
@@ -193,4 +193,4 @@ def delete_product(id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all() # creates all tables
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
